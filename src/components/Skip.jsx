fd8286a622497637b233259c8b40a273306f1f1c
Hannah <hannah1155@naver.com>
2023-10-31 21:29:17 +0900

    빌드 ㅜ정

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import imagesLoaded from "imagesloaded";
 
 const Skip = () => {
-
   const [current, setCurrent] = useState(0);
   const [loading, setLoading] = useState(true);
 
@@ -10,9 +9,8 @@ const Skip = () => {
     const imgLoad = imagesLoaded("body");
     const imgTotal = imgLoad.images.length;
 
-    // 이미지가 하나도 없는 경우에 대한 체크
     if (imgTotal === 0) {
-      setCurrent(100); // 이미지가 없다면 로딩은 완료된 것으로 간주
+      setCurrent(100);
       setLoading(false);
       return;
     }
@@ -25,13 +23,14 @@ const Skip = () => {
       setCurrent((prev) => prev + (target - prev) * 0.1);
     }, 2000 / 60);
 
-    imgLoad.on("progress", () => {
-      // 이미지 로딩 중에 처리할 작업이 있다면 여기에서 처리할 수 있습니다.
+    imgLoad.on("always", () => {
+      // 이미지 로딩이 완료된 후에 처리할 작업을 여기에 추가할 수 있습니다.
+      clearInterval(progressTimer);
+      setLoading(false);
     });
 
     return () => {
       clearInterval(progressTimer);
-      setLoading(false); // 컴포넌트가 언마운트될 때 로딩 상태를 false로 설정하여 컴포넌트를 사라지게 합니다.
     };
   }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행되도록 합니다.
 
@@ -44,7 +43,6 @@ const Skip = () => {
     );
   }
 
-  // 로딩이 완료된 후에는 null을 반환하여 컴포넌트가 렌더링되지 않도록 합니다.
   return null;
 };
 
