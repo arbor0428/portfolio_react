9a3299ed70352e084d46f65693e8e5c23c44f5c0
Hannah <hannah1155@naver.com>
2023-10-31 20:42:08 +0900

    빌드

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,49 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Intro = () => {
-    //typing
-    const [content, setContent] = useState("Problem Solver \n Hana Park.");
-    const [displayedText, setDisplayedText] = useState('');
-    let i = 0;
+  const [content, setContent] = useState("Problem Solver\nHana Park.");
+  const [displayedText, setDisplayedText] = useState('');
+  let i = 0;
+  let intervalId = null;
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            let txt = content[i++];
-            setDisplayedText(prevText => prevText + (txt === "\n" ? "<br/>" : txt));
-            if (i > content.length) {
-                setDisplayedText('');
-                i = 0;
-            }
-        }, 200);
+  useEffect(() => {
+    intervalId = setInterval(() => {
+      let txt = content[i++];
+      setDisplayedText(prevText => prevText + (txt === "\n" ? "\n" : txt));
+      if (i >= content.length) {
+        clearInterval(intervalId);
+      }
+    }, 200);
 
-        return () => clearInterval(intervalId);
-    }, [content]);
-
-    const handleScroll = () => {
-        const targetElement = document.getElementById('port');
-        scroll.scrollTo(targetElement.offsetTop, {
-            duration: 1000,
-            smooth: 'easeInOutQuart',
-        });
+    return () => {
+      clearInterval(intervalId);
     };
+  }, [content]);
 
-    return (
-        <section id="intro">
-            <div className="intro__center">
-                <div className="intro__text">
-                    <span className="text" dangerouslySetInnerHTML={{ __html: displayedText }} />
-                    <span className="blink">|</span>
-                </div>
-                <p className="info phone">contact me. 010-5220-2107</p>
-                <a href="https://github.com/whynot1217" className="info mail" target="_blank" rel="noopener noreferrer">https://github.com/arbor0428</a>
-                <Link to="port" smooth={true} duration={1000} className="scrolldown" onClick={handleScroll}>
-                    <p>scrolldown</p>
-                    <div className="tri"></div>
-                </Link>
-            </div>
-        </section>
-    );
+  const handleScroll = () => {
+    scroll.scrollTo(window.innerHeight, {
+      duration: 1000,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
+  return (
+    <section id="intro">
+      <div className="intro__center">
+        <div className="intro__text">
+          <span className="text">{displayedText}</span>
+          <span className="blink">|</span>
+        </div>
+        <p className="info phone">contact me. 010-5220-2107</p>
+        <a
+          href="https://github.com/arbor0428"
+          className="info mail"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/arbor0428
+        </a>
+        <div className="scrolldown" onClick={handleScroll}>
+          <p>scrolldown</p>
+          <div className="tri"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
