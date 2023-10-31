6e8458e8ce56b83f4254c94082d3fcfa1c3c96a2
Hannah <hannah1155@naver.com>
2023-10-31 21:24:22 +0900

    빌드 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 
 const Intro = () => {
